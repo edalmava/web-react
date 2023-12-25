ff922e3b9a4799f2515bc6cb7f0415e0342c49e3
edalmava <eamartinezv@gmail.com>
2023-12-25 16:49:36 -0500

    Modificado el juego de tres en línea

--- src/app/juegos/page.js
+++ src/app/juegos/page.js
@@ -10,6 +10,32 @@ export default function Board() {
   const [xIsNext, setXIsNext] = useState(true);
   const [squares, setSquares] = useState(Array(9).fill(null));
 
+  function Header() {
+    return (
+      <header className="p-3 bg-zinc-900 text-zinc-200 font-bold flex justify-between leading-6 content-center">
+        <h1 className="text-4xl px-2">
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 inline-block">
+            <path strokeLinecap="round" strokeLinejoin="round" d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10" />
+          </svg>
+          Tres en Línea</h1>      
+        <nav className="p-2 inline-block">
+          <ul>
+            <li className='inline mx-5'><a href="/">Inicio</a></li>
+            <li className='inline'><a href="/juegos">Juegos</a></li>
+          </ul>
+        </nav>
+      </header>
+    )
+  }
+
+  function Footer() {
+    return (
+      <footer className="bg-zinc-900 text-zinc-200 text-xs absolute bottom-0.5">
+        Elaborado por Edalmava con ❤️
+      </footer>
+    )
+  }
+
   function handleClick(i) {
     if (squares[i] || calculateWinner(squares)) {
       return;
@@ -33,8 +59,10 @@ export default function Board() {
   }
 
     return (
-      <>
-        <div className="status">{status}</div>
+      <div className="container p-4">
+        <Header />
+    
+        <div className="status m-5">{status}</div>
         <div className="board-row">
           <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
           <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
@@ -50,7 +78,9 @@ export default function Board() {
           <Square value={squares[7]} onSquareClick={() => handleClick(7)} />
           <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
         </div>
-      </>   
+
+        <Footer />
+      </div>   
     ) 
   }
 
